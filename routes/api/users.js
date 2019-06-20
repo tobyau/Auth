1330034ea8cdddba91342c43d817943bf21ef5c0
Toby <tobyau9000@gmail.com>
2019-06-20 19:45:25 -0400

    set up login,register, and api endpoints

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,41 +12,89 @@ const validateLoginInput = require("../../validation/login");
 const User = require("../../models/User");
 
 // @route POST api/users/register
-// @desc Register user
-// @access Public
+// Register user
+// Public access
 router.post("/register", (req, res) => {
-  // validate register input
+  // Form validation
   const { errors, isValid } = validateRegisterInput(req.body);
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  // check user email
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    }
+    else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+// Hash password before saving to database
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
-  // check validation
-  if(!isValid) {
+
+// @route POST api/users/login
+// Login user and return JWT token
+// Public access
+router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
     return res.status(400).json(errors);
   }
 
-  // find user in database by email 
-  User.findOne({ email: req.body.email })
-    .then(user => {
-      if(user) {
-        return res.status(400).json({ email: "Email already exists" });
-      }
-      else {
-        const newUser = new User({
-          name: req.body.name,
-          email: req.body.email,
-          password: req.body.password
-        });
+  const email = req.body.email;
+  const password = req.body.password;
 
-        // hash pw before saving to db
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err,hash) => {
-            if(err) throw err;
-            newUser.password = hash;
-            newUser.save()
-              .then(user => res.json(user))
-              .catch(err => console.log(err));
-          });
-        });
-      }
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ emailnotfound: "Email not found" });
+    }
 
+    // Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // User matched, create JWT Payload
+        const payload = {
+          id: user.id,
+          name: user.name
+        };
+        // Sign token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          {
+            expiresIn: 31556926 // 1 year in seconds
+          },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      } else {
+        return res
+          .status(400)
+          .json({ passwordincorrect: "Password incorrect" });
+      }
+    });
   });
-};
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require("./routes/api/users");
 
 const app = express();
 
@@ -19,6 +22,15 @@ const db = require('./config/keys').mongoURI;
 mongoose.connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000
 
