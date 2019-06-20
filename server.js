93283337b68a192e27bffa706cc52d26c2d45951
Toby <tobyau9000@gmail.com>
2019-06-20 16:50:44 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// body-parser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// connect to MongoDB
+mongoose.connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
