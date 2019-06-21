fe03e43e6d28f53507b03a9d6195008a38f7b14b
Toby <tobyau9000@gmail.com>
2019-06-21 14:58:38 -0400

    front-end

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
