97d78111003a52bf6e1c8335bc1d7b4b533cbce0
Toby <tobyau9000@gmail.com>
2019-06-21 15:35:57 -0400

    add login/register and landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -38,7 +38,7 @@ class Landing extends Component {
                   borderRadius: "3px",
                   letterSpacing: "1.5px"
                 }}
-                className="btn btn-large btn-flat waves-effect white black-text"
+                className="btn btn-large waves-effect white black-text"
               >
                 Log In
               </Link>
